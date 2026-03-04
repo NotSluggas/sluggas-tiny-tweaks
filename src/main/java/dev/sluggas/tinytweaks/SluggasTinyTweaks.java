b91d735a84e26912e9c8f3a74521d107bb9867f4
NotSluggas <altlucaslmckinney@outlook.com>
2026-03-03 20:57:56 -0600

    ore generates

--- src/main/java/dev/sluggas/tinytweaks/SluggasTinyTweaks.java
+++ src/main/java/dev/sluggas/tinytweaks/SluggasTinyTweaks.java
@@ -1,6 +1,13 @@
 package dev.sluggas.tinytweaks;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.gen.GenerationStep;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,5 +23,11 @@ public class SluggasTinyTweaks implements ModInitializer {
 
 		ModBlocks.registerModBlocks();
 		ModItems.registerModItems();
+
+		BiomeModifications.addFeature(
+				BiomeSelectors.includeByKey(BiomeKeys.END_HIGHLANDS, BiomeKeys.END_MIDLANDS, BiomeKeys.END_BARRENS),
+				GenerationStep.Feature.UNDERGROUND_ORES,
+				RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of("sluggas_tiny_tweaks", "end_shard_ore_small"))
+		);
 	}
 }

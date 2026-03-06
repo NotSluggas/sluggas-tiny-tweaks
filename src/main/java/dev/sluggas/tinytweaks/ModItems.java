d7ade9854f2f2f1a18aceccbd8318d6269fac6bc
NotSluggas <altlucaslmckinney@outlook.com>
2026-03-06 12:46:49 -0600

    added Grilled Carrot

--- src/main/java/dev/sluggas/tinytweaks/ModItems.java
+++ src/main/java/dev/sluggas/tinytweaks/ModItems.java
@@ -1,6 +1,7 @@
 package dev.sluggas.tinytweaks;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
@@ -15,6 +16,10 @@ public class ModItems {
     public static final Item END_SHARD = registerItem("end_shard", new Item(new Item.Settings()
             .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(SluggasTinyTweaks.MOD_ID, "end_shard")))));
 
+    public static final Item GRILLED_CARROT = registerItem("grilled_carrot", new Item(new Item.Settings()
+            .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(SluggasTinyTweaks.MOD_ID, "grilled_carrot")))
+            .food(new FoodComponent.Builder().nutrition(4).saturationModifier(1.0f).build())));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM,
                 RegistryKey.of(RegistryKeys.ITEM, Identifier.of(SluggasTinyTweaks.MOD_ID, name)),
@@ -22,9 +27,13 @@ public class ModItems {
     }
 
     public static void registerModItems() {
-        //add to Creative inv
+// Add to creative inv
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
             content.addAfter(Items.ANCIENT_DEBRIS, END_SHARD);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(content -> {
+            content.addAfter(Items.CARROT, GRILLED_CARROT);
         });
 
     }
